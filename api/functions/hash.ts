2d055fb7097562446711726a45a7f492ad5f0363
iamMAHAM <abdulkabore@gmail.com>
2022-11-20 17:16:37 +0000

    uhash

--- api/functions/hash.ts
+++ api/functions/hash.ts
@@ -1,9 +1,14 @@
 import bcrypt from "bcrypt"
 
-const hash = async (string: string): Promise<string> => {
+export const hash = async (string: string): Promise<string> => {
     const salt = await bcrypt.genSalt(10)
     const hs = await bcrypt.hash(string, salt )
     return hs
 }
 
-export default hash
+export const comparePassword = (current: string, hashed: string, next:Function) => {
+    bcrypt.compare(current, hashed, (err, isMatch) =>{
+        if (err) return next(err)
+        next(null, isMatch)
+    })
+}
