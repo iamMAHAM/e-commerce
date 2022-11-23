d98d7e65e80614db7a9a9c0fe3665ad8e2648211
iamMAHAM <abdulkabore@gmail.com>
2022-11-23 15:08:58 +0000

    update admin controllers and methods

--- api/controllers/admin.controller.ts
+++ api/controllers/admin.controller.ts
@@ -1,1 +1,152 @@
-import { Router, Request, Response} from "express";
+import { Request, Response} from "express";
+import { BAD_REQUEST, INTERNAL, NOT_FOUND, SUCCESS, UNAUTHORIZED } from "../configs/defs";
+import { accessToken, decode, encode } from "../functions/sign";
+import Admin, { iAdmin } from "../models/admin.model"
+
+/** A controller for admin api */
+class AdminController{
+
+    /**
+     * Adds an admin information to database
+     * @params {Request} req - the request handler
+     * @params {Response} res - the response handler
+     */
+    static async addAdmin(req: Request, res: Response): Promise<void>{
+        const data: iAdmin = req.body
+        if (!data) res.status(401).json(UNAUTHORIZED)
+        try{
+            const user = new Admin(data)
+            console.log(user)
+            await user.save()
+            res.status(200).json(SUCCESS)
+        } catch (e: unknown){
+            res.status(200).json({ 
+                status: false,
+                message: e instanceof Error ? e.message : String(e)
+            })
+        }
+    }
+    /**
+     * Retrieves an admin information
+     * @params {Request} req - the request handler
+     * @params {Response} res - the response handler
+     */
+    static async getAdmin(req: Request, res: Response): Promise<void>{
+        const id = req.params.id
+        const auth = req.headers.authorization
+
+        if (!id || !auth || !decode(auth)){ // invalid paramss of invalid token
+            res.status(401).json(UNAUTHORIZED)
+            return
+        }
+
+        const admin = await Admin.findById(id).select('-password')
+        console.log(admin)
+        if (!admin) {
+            res.status(404).json(NOT_FOUND)
+            return
+        }
+
+        res.status(200).json({
+            ...SUCCESS,
+            message: encode({...admin.toObject()}),
+            access_token: accessToken()
+        })
+    }
+
+
+    /**
+     * Authenticates an admin
+     * @params {Request} req - the request handler
+     * @params {Response} res - the response handler
+     */
+    static async loginAdmin(req: Request, res: Response): Promise<void>{
+        const adminname = req.params.adminname
+        const password = req.params.password
+        const admin = await Admin.findOne({ adminname: adminname})
+
+        if (!admin?.validatePassword(password)){ // invalid params or invalid token
+            res.status(401).json({
+                ...UNAUTHORIZED,
+                message: 'Bad credentials'
+            })
+            return
+        }
+
+        res.status(200).json({
+            ...SUCCESS,
+            message: encode({...admin.toObject()}),
+            access_token: accessToken()
+        })
+    }
+
+
+    /**
+     * Updates an admin informations
+     * @params {Request} req - the request handler
+     * @params {Response} res - the response handler
+     */
+    static async updateAdmin(req: Request, res: Response): Promise<void>{
+
+        const data = req.body
+        const id: string = req.params.id
+        const payload = req.headers.authorization
+        
+        if (!id || !data || !payload || !decode(payload)){
+            res.status(401).json(UNAUTHORIZED)
+            return
+        }
+        try{
+            const admin = await Admin.findById(id)
+            if (!admin) throw new Error('admin not found' )
+            console.log(data)
+            if (await admin.validatePassword(data.cpassword || '')){ //confirm admin identity
+                if (data.password){
+                    admin.password = data.password
+                    await admin.save()
+                }
+                await admin.updateOne({...data, password: undefined})
+                // await admin.save()
+                res.json({
+                    ...SUCCESS,
+                    access_token: accessToken()
+                })
+            } else throw new Error('password is invalid')
+        } catch (e){
+            console.log(e)
+            res.json({
+                status: false,
+                message: e instanceof Error ? e.message : String(e)
+            })
+        }
+    }
+    /**
+     * Deletes an admin from database
+     * @params {Request} req - the request handler
+     * @params {Response} res - the response handler
+     */
+    static async deleteAdmin(req: Request, res: Response): Promise<void>{
+        const id = req.params.id
+        const authorization = req.headers.authorization
+        if (!id || !authorization || !decode(authorization)){
+            res.status(400).json(BAD_REQUEST)
+            return
+        }
+        const decoded = decode(authorization)
+        const ownerId = (await Admin.findOwner())._id.toHexString()
+
+        console.log(decoded, ownerId)
+        if (ownerId !== decoded || ownerId === id) {
+            res.status(401).json(UNAUTHORIZED)
+            return
+        }
+        try{
+            await Admin.findByIdAndDelete(id)
+            res.status(200).json(SUCCESS)
+        } catch (e){
+            res.status(500).json(INTERNAL)
+        }
+    }
+}
+
+export default AdminController
---
+++ api/models/admin.model.ts
@@ -0,0 +1,128 @@
+import { Schema, model, Model, HydratedDocument } from "mongoose";
+import { comparePassword, hash } from "../functions/hash";
+import { iUser, iUserMethods } from "./user.model"
+import validator from "validator";
+
+/**
+ * an admin properies interfaces definitions
+ * that inherit from user model excepts carts prop
+ */
+export interface iAdmin extends Omit<iUser, 'carts'>{
+    permissions: string[],
+    owner?: boolean
+}
+
+/**
+ * a admin methods extends of user methods
+ */
+interface iAdminMethods extends iUserMethods{
+    /**
+     * checks if current user is a owner
+     */
+    isOwner(): Promise<Boolean>
+}
+
+interface adminModel extends Model<iAdmin, {}, iAdminMethods>{
+    /**
+     * find a owner from Admin collections
+     */
+    findOwner(): Promise<HydratedDocument<iAdmin, iAdminMethods>>
+}
+
+/**
+ * an admin schema
+ */
+const aSchema = new Schema<iAdmin, adminModel, iAdminMethods>({
+    permissions: {
+        type: [String],
+        required: true,
+        default: ['basic']
+    },
+    owner: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    fullname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: [true, 'missing username'],
+        minLength: [4, 'Minimum 4 caracteres.'],
+        maxLength: [16, 'Maximum 16 caracteres.'],
+        validate: {
+            validator: (v: string) => validator.isAlpha(v, 'fr-FR'),
+            message: 'Only letter are allowed'
+        }
+    },
+    email: {
+        type: String,
+        required: [true, 'email is required'],
+        unique: true,
+        trim: true,
+        validate: {
+            validator: (v: string) => validator.isEmail(v),
+            message: 'invalid email address'
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'password is required'],
+        validate: {
+            validator: (v: string) => validator.isStrongPassword(v),
+            message: 'password is not strong'
+        }
+    },
+    birthday: {
+        type: Date,
+        required: [true, 'missing birthday date'],
+        min: ['1900-01-01', 'invalid date'],
+        max: ['2005-12-31', '18 years minimum'],
+    },
+    emailVerified: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+    avatar: {
+        type: String,
+        required: false,
+        default: 'https://img2.freepng.fr/20180331/eow/kisspng-computer-icons-user-clip-art-user-5abf13db298934.2968784715224718991702.jpg' 
+    },
+}, {
+    timestamps: true
+})
+
+aSchema.pre('save', async function (next: Function){
+    try{
+        const user = this
+        if (!user.isModified('password')) return next()
+        this.password = await hash(this.password!)
+        return next()
+    } catch(e: unknown){
+        return next(e)
+    }
+})
+
+aSchema.methods.validatePassword = async function (password: string): Promise<Boolean>{
+    const isValid = await comparePassword(password, this.password)
+    return isValid
+        ? true
+        : false
+}
+
+aSchema.statics.findOwner = async function (){
+    return await this.findById('637e35e0b5f9ffdf6d7ea7df')
+}
+
+aSchema.methods.isOwner = async function(): Promise<boolean>{
+    return this.owner === true
+}
+
+
+aSchema.addListener('change', ()=> console.log('admin changed'))
+
+export default model<iAdmin, adminModel>('Admin', aSchema)
