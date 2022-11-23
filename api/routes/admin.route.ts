67dd867f81b89622fa92cd63ff68f75d77635a73
iamMAHAM <abdulkabore@gmail.com>
2022-11-23 15:09:13 +0000

    update admiN routes

--- api/routes/admin.route.ts
+++ api/routes/admin.route.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from "express";
+import AdminController from "../controllers/admin.controller";
 
 const router = Router()
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('ok')
-})
+router.post('/add', AdminController.addAdmin)
+router.get('/:id', AdminController.getAdmin)
+router.post('/login', AdminController.loginAdmin)
+router.put('/update/:id', AdminController.updateAdmin)
+router.delete('/:id', AdminController.deleteAdmin)
 
 export default router
